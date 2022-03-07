674c0ec7fbfbd0e69bd1d04080327ccab12d4861
Madi Lippmann <madi.lippmann@gmail.com>
2022-03-06 19:59:55 -0800

    Save before merge to main

--- frontend/src/components/Home/index.js
+++ frontend/src/components/Home/index.js
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Redirect, Link } from 'react-router-dom';
+import { Redirect, Link, useHistory } from 'react-router-dom';
 
 import * as notesActions from '../../store/notes';
 import { formattedDate, sortByUpdatedAt, shortenedContent } from '../utils/utils.js';
+
+import { UilPlusCircle } from '@iconscout/react-unicons';
+
 
 import * as SC from './StyledComponents.js';
 import { OuterDiv } from '../utils/utils.js'
@@ -13,6 +16,7 @@ export default function Home() {
     const dispatch = useDispatch();
     const sessionUser = useSelector(state => state.session.user);
     const notes = useSelector(state => state.notes.notes);
+    const history = useHistory();
 
     useEffect(() => {
         dispatch(notesActions.loadNotesThunk(sessionUser.id))
@@ -32,6 +36,11 @@ export default function Home() {
 
     formattedNotes = sortByUpdatedAt(formattedNotes).slice(0, 6);
 
+    const addNote = async () => {
+        const noteId = await dispatch(notesActions.createNoteThunk(sessionUser.id))
+        history.push(`/notes/${noteId}`)
+        return <Redirect to={`/notes/${noteId}`} />
+    }
 
     return (
         <OuterDiv className='main' style={{ padding: '50px', display: 'flex', flexDirection: 'column' }}>
@@ -40,13 +49,19 @@ export default function Home() {
                 <SC.NotesContainer>
                     {formattedNotes.map(note => (
                         <Link to={`/notes/${note.id}`} >
-                            <div key={note.id} className='square-card note-card' style={{ border: 'none' }}>
+                            <div key={note.id} className='square-card note-card'>
                                 <h3 className='note-card-title'>{note.title}</h3>
                                 <p className='note-card-content'>{note.content}</p>
                                 <p className='note-card-date'>{note.updatedAt}</p>
                             </div>
                         </Link>
                     ))}
+                    <button type='button' onClick={addNote} style={{ border: 'none' }}>
+                        <div className='create-note-card square-card home-page'>
+                            <UilPlusCircle size='75' />
+                            <h2 className='add-card-title'>New Note</h2>
+                        </div>
+                    </button>
                 </SC.NotesContainer>
             </SC.Backdrop>
 
